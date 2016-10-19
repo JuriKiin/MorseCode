0f4b16634b96a3fa95cb52fc60e3ec6ff01991f8
JuriKiin <cornfield24@gmail.com>
2016-10-19 14:18:20 -0400

    Add files via upload

---
+++ MorseDictionary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MorseCodeTranslator
+{
+    class MorseDictionary
+    {
+        public Dictionary<char, string> morseCodeDictionary = new Dictionary<char, string>();
+
+        public MorseDictionary()
+        {
+            morseCodeDictionary.Add('A',"o-");
+            morseCodeDictionary.Add('B', "-ooo");
+            morseCodeDictionary.Add('C', "-o-o");
+            morseCodeDictionary.Add('D', "-oo");
+            morseCodeDictionary.Add('E', "o");
+            morseCodeDictionary.Add('F', "oo-o");
+            morseCodeDictionary.Add('G', "--o");
+            morseCodeDictionary.Add('H', "oooo");
+            morseCodeDictionary.Add('I', "oo");
+            morseCodeDictionary.Add('J', "o---");
+            morseCodeDictionary.Add('K', "-o-");
+            morseCodeDictionary.Add('L', "o-oo");
+            morseCodeDictionary.Add('M', "--");
+            morseCodeDictionary.Add('N', "-o");
+            morseCodeDictionary.Add('O', "---");
+            morseCodeDictionary.Add('P', "o--o");
+            morseCodeDictionary.Add('Q', "--o-");
+            morseCodeDictionary.Add('R', "o-o");
+            morseCodeDictionary.Add('S', "ooo");
+            morseCodeDictionary.Add('T', "-");
+            morseCodeDictionary.Add('U', "oo-");
+            morseCodeDictionary.Add('V', "ooo-");
+            morseCodeDictionary.Add('W', "o--");
+            morseCodeDictionary.Add('X', "-oo-");
+            morseCodeDictionary.Add('Y', "-o--");
+            morseCodeDictionary.Add('Z', "--oo");
+            morseCodeDictionary.Add('1', "o----");
+            morseCodeDictionary.Add('2', "oo---");
+            morseCodeDictionary.Add('3', "ooo--");
+            morseCodeDictionary.Add('4', "oooo-");
+            morseCodeDictionary.Add('5', "ooooo");
+            morseCodeDictionary.Add('6', "-oooo");
+            morseCodeDictionary.Add('7', "--ooo");
+            morseCodeDictionary.Add('8', "---oo");
+            morseCodeDictionary.Add('9', "----o");
+            morseCodeDictionary.Add('0', "-----");
+            morseCodeDictionary.Add(' ',"*");
+
+        }
+        public char GetKey(string input)
+        {
+            char key = 'A';
+            foreach(KeyValuePair<char,string> pair in morseCodeDictionary)
+            {
+                if(pair.Value == input)
+                {
+                    key = pair.Key;
+                }
+            }
+
+            return key;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MorseCodeTranslator
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            MorseDictionary morDict = new MorseDictionary();
+            string userInput = "";
+            string morseString = "";
+
+            Console.WriteLine("X--------------------------------------------X");
+            Console.WriteLine("Welcome to the MorseCode - English Translator!");
+            Console.WriteLine("Author: Juri Kiin");
+            Console.WriteLine("Created: October 7 2016");
+            Console.WriteLine();
+            Console.WriteLine("Simply type in either morse code or english.");
+            Console.WriteLine("When typing in morse code, seperate letters by a space");
+            Console.WriteLine("And seperate words by *");
+            Console.WriteLine("X--------------------------------------------X");
+
+
+            while(true)
+            {
+                morseString = "";   //Reset the morse string every time.
+                Console.Write("English or Morse Code (Type E / M): ");
+                userInput = Console.ReadLine();
+                string userString = userInput.ToUpper();
+
+                if (userString == "E")
+                {
+                    Console.Write("Type in English: ");
+
+                    string english = "";
+                    english = Console.ReadLine();
+
+                    string englishInput = english.ToUpper();
+
+                    //We are using a English
+                    foreach (char c in englishInput)
+                    {
+                        if (morDict.morseCodeDictionary.ContainsKey(c))
+                        {
+                            morseString += morDict.morseCodeDictionary[c] + " ";
+                        }
+                        else
+                        {
+                            morseString += c;
+                        }
+
+                    }
+
+                    Console.WriteLine(morseString);
+                }
+                else if(userString == "M")
+                {
+                    Console.Write("Type in Morse Code: ");
+
+                    string morse = Console.ReadLine();
+                    //We are using Morse Code
+                    string[] splits = morse.Split(' ');
+
+                    foreach (string s in splits)
+                    {
+                        if (morDict.morseCodeDictionary.ContainsValue(s))
+                        {
+                            morseString += morDict.GetKey(s);
+                        }
+                        else
+                        {
+                            morseString += s;
+                        }
+                    }
+
+                    Console.WriteLine(morseString);
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized input. Please type either E or M.");
+                    continue;
+                }
+            }
+        }
+    }
+}
